e07538d949a1d162a680bace8527611349866165
Saiful islam <saifulislam265@gmail.com>
2018-10-15 11:48:22 +0600

    Added Bootstrap

--- index.php
+++ index.php
@@ -5,39 +5,44 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/screen.css">
 
 </head>
 <body>
 
-<div class="container">
-    <h2>Stacked form</h2>
-    <form action="action_page.php" method="post">
+<div class="container ">
+	
+	    <h2>Validatin Form</h2>
+		    <form class="cmxform" id="commentForm" action="action_page.php" method="post">
 
-        <div class="form-group">
-            <label for="email">First Name:</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-        </div>
+		        <div class="form-group col-sm-6">
+		            <label for="email">Name:</label>
+		            <input type="text" class="form-control" id="name cname" placeholder="Enter Name" name="name" minlength="2" required>
+		        </div>
 
-        <div class="form-group">
-            <label for="email">Last Name:</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-        </div>
+		        <div class="form-group col-sm-6">
+		            <label for="email">Email:</label>
+		            <input type="email" class="form-control" id="cemail" placeholder="Enter email" name="email" required>
+		        </div>
 
-        <div class="form-group">
-            <label for="email">Email:</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-        </div>
-        <div class="form-group">
-            <label for="pwd">Password:</label>
-            <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
-        </div>
-        <div class="form-group form-check">
-            <label class="form-check-label">
-                <input class="form-check-input" type="checkbox" name="remember"> Remember me
-            </label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+		        <div class="form-group col-sm-6">
+		            <label for="url">Website:</label>
+		            <input type="url" class="form-control" id="url" placeholder="Enter web URL" name="url" required>
+		        </div>
+
+		        <div class="form-group col-sm-6">
+		            <label for="pwd">Password:</label>
+		            <input type="password" class="form-control form-control" id="cpassword" placeholder="Enter password" name="password" required>
+		        </div>
+
+		        <div class="form-group col-sm-6">
+		            <label for="pwd">COMMENT:</label>
+		            <textarea rows="5" class="form-control" id="ccomment" placeholder="Type Your Comment" name="comment" required></textarea>
+		        </div>
+
+		        <button type="submit" class="btn btn-primary mb-2">Submit</button>
+		    </form>
+
 </div>
 <script src="assets/js/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
